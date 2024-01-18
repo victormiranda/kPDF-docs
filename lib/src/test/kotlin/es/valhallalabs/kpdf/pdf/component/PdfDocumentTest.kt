80de57365cb581928bedc3b98cb4ab296014d380
Victor Miranda <victormiranda@valhalla-labs.es>
2024-01-18 11:43:54 +0000

    Add tests for PdfDocument class
    This commit adds a new test suite for the PdfDocument class in Kotlin. It includes checks for basic A4 document object page size, object inner container size and access to local classpath document resources.

---
+++ lib/src/test/kotlin/es/valhallalabs/kpdf/pdf/component/PdfDocumentTest.kt
@@ -0,0 +1,54 @@
+package es.valhallalabs.kpdf.pdf.component
+
+import es.valhallalabs.kpdf.api.component.boxmodel.BoxModel
+import es.valhallalabs.kpdf.api.component.boxmodel.PageFormat
+import es.valhallalabs.kpdf.api.component.document.DocumentProperties
+import es.valhallalabs.kpdf.api.component.document.LocalDocumentResource
+import es.valhallalabs.kpdf.api.component.page.FullContainerPage
+import es.valhallalabs.kpdf.api.component.section.DocumentSection
+import org.assertj.core.api.Assertions.assertThat
+
+import org.junit.jupiter.api.Test
+import java.time.Instant
+
+internal class PdfDocumentTest {
+
+	private val document: PdfDocument = PdfDocument(
+		properties = DocumentProperties(requestedAt = Instant.now(), tenantId = "23", pageFormat = PageFormat.PAGE_BASE_A4),
+		resources = listOf(
+			LocalDocumentResource(resourceName = "Acme font", resourceLocation = "fonts/acme.ttf")
+		),
+		sections =  listOf(
+			DocumentSection(
+				box = PageFormat.PAGE_BASE_A4,
+				pages = listOf(
+					FullContainerPage(
+						box = PageFormat.PAGE_BASE_A4.innerBox as PageFormat
+					)
+				)
+			)
+		)
+	)
+
+	@Test fun `check basic A4 document object page size `(){
+		val a4BoxModel: BoxModel = document.box
+
+		assertThat(a4BoxModel.width.points).isEqualTo(595.2756f)
+		assertThat(a4BoxModel.height.points).isEqualTo(841.88983f)
+	}
+
+	@Test fun `check basic A4 document object inner container size `(){
+		val firstPage: FullContainerPage = document.sections[0].pages[0] as FullContainerPage
+
+		assertThat(firstPage.box.width.points).isEqualTo(523.2756f)
+		assertThat(firstPage.box.height.points).isEqualTo(769.88983f)
+	}
+
+	@Test fun `check access to local classpath document resources`(){
+        val resource = document.resources[0]
+
+		assertThat(resource.resource).exists()
+	}
+
+
+}
